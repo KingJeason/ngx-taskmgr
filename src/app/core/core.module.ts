35894d271786167cd1fe74519c01b64736ac3a78
KingJeason <1503268691@qq.com>
2018-10-09 02:16:59 +0800

    基本布局

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,18 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
-import { CommonModule } from '@angular/common'
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component'
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule
   ],
-  declarations: []
+  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
+  // 这里如果不导出的话, 则只能在core模块里使用
+  exports: [HeaderComponent, FooterComponent, SidebarComponent]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() parent: CoreModule) {
