810096a04b6df2c4e126f85ceb7542d8e6250235
KingJeason <1503268691@qq.com>
2018-10-14 02:09:11 +0800

    key frame 基础知识

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { OverlayContainer } from '@angular/cdk/overlay'
-import { trigger, state, transition, style, animate } from '@angular/animations'
+import { trigger, state, transition, style, animate, keyframes } from '@angular/animations'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,18 +12,29 @@ import { trigger, state, transition, style, animate } from '@angular/animations'
           {
             'background-color': 'green',
             'height': '100px',
-            'transform': 'translate(0)'
+            'transform': 'translateY(-100%)'
           }
         ])),
         state('red', style([
           {
             'background-color': 'red',
-            'height': '50px',
-            'transform': 'translate(100%)'
+            'height': '100px',
+            'transform': 'translateY(100%)'
           }
         ])),
-        transition('green => red', animate('.2s 1s')),
-        transition('red => green', animate(1000))
+        transition('green => red', animate('.8s ease-in')),
+        transition('red => green', animate(5000, keyframes([
+          style({ transform: 'translateY(100%)' }),
+          style({ transform: 'translateY(50%)' }),
+          style({ transform: 'translateY(40%)' }),
+          style({ transform: 'translateY(20%)' }),
+          style({ transform: 'translateY(0%)' }),
+          style({ transform: 'translateY(-10%)' }),
+          style({ transform: 'translateY(-2%)' }),
+          style({ transform: 'translateY(0%)' }),
+          style({ transform: 'translateY(100%)' }),
+          style({ transform: 'translateY(-100%)' }),
+        ])))
       ])
   ]
 })
